631fec460dc2fafb3658336b01bb7b4974088ba5
leetmikeal <leetmikeal@example.com>
2013-04-08 11:02:04 +0900

    can use

--- sortfiledate.py
+++ sortfiledate.py
@@ -4,30 +4,33 @@
 import os
 import sys
 import subprocess
+import time
 
+
+def toDateString(n):
+  #return str(n[0])+str(n[1]).rjust(2,"0")+str(n[2]).rjust(2,"0")
+  return time.strftime("%Y/%m/%d %H:%M:%S", time.localtime(n))
+
+def printHelp():
+  print "Usage:"
+  print "  python %s <find file regular expression>" % __file__
 
 if __name__ == '__main__':
   argv = sys.argv  
   argc = len(argv)
   if argc > 2:
-    exit()
+    printHelp()
+    exit(1)
   elif argc == 2:
+    if argv[1] == '-help':
+      printHelp()
+      exit()
     findre = argv[1]
   else:
     findre = "*"
 
-  # execute command
-  cwd = "/"
-  #cmd = ['find', './', '-type', 'f']
-  #subprocess_args = { 'stdin': subprocess.PIPE,
-  #    'stdout': subprocess.PIPE,
-  #    'stderr': subprocess.STDOUT,
-  #    'cwd': cwd,
-  #    'close_fds': True,
-  #    }
-  cmd = 'find ./ -type f'
+  cmd = 'find ./ -type f -name "%s"' % (findre)
   try:
-    #p = subprocess.Popen(cmd, **subprocess_args)
     p = subprocess.Popen(cmd,
         shell=True,
         stdin=subprocess.PIPE,
@@ -38,15 +41,17 @@ if __name__ == '__main__':
     print "Failed to execute command: %s" % cmd[0]
     sys.exit(1)
 
+  filelist = []
   stdout = p.stdout
   for line in stdout:
-    print line.strip()
-  exit()
-  #(stdout, stderr) = p.communicate()
-  #(stdouterr, stdin) = (p.stdout, p.stdin)
-  print "-" * 80
-  print stdout
-  #for line in stdouterr.readline():
-  #  print line,
-  print "-" * 80
-  print "Retrun code: %d" % p.wait()
+    updatetime = os.stat(line.strip()).st_mtime
+    filelist.append(
+        (updatetime, line.strip())
+        )
+  #sorted(filelist, key=lambda file: file[1]) # sort by datetime
+  filelist.sort() # sort by datetime
+
+  for file in filelist:
+    #print "%10s %s" % (toDateString(file[1]), file[0])
+    print "%20s %s" % (toDateString(file[0]), file[1])
+    
